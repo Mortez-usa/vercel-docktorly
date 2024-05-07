74710b4ba125e2f338e1eb2594e491422925aaec
Mortez-usa <hatam.info@gmail.com>
2024-05-06 22:25:50 -0700

    toast changed

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -20,8 +20,6 @@ const Contact = () => {
         body: JSON.stringify(emailData),
       });
       if (response.ok && response.status === 200) {
-        <Toaster/>
-        {() => {toast.success('Your message has been sent successfully');}}
         console.log("Email sent!");
         location.reload();
       } else {
@@ -118,10 +116,11 @@ const Contact = () => {
                       ></textarea>
                     </div>
                   </div>
+                  <Toaster/>
                   <div className="w-full px-4">
                     <button
                       type="submit"
-                      onClick={handleSubmit}
+                      onClick={() => {toast.error("Your message has not been sent");handleSubmit;}}
                       className="rounded-3xl bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
                     >
                       Submit
